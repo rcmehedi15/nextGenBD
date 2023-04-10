d2fd0c10c77ecf9c7efee2cf9881fc306e32ea5e
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-04-10 17:19:03 +0600

    header finish

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,11 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Header from './components/Header';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Header></Header>,
   },
 ]);
 
