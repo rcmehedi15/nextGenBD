9433e187a3907bb134f7b9beea458282e0172947
rcmehedi15 <hmdmehedi2016@gmail.com>
2023-04-12 13:16:25 +0600

    featured done

--- src/components/FeaturedJobs.jsx
+++ src/components/FeaturedJobs.jsx
@@ -1,26 +1,28 @@
 import React from 'react';
 
-const FeaturedJobs = () => {
+const FeaturedJobs = ({job}) => {
+    const {logo,job_title,job_type,job_time,address,company,salary} = job;
     return (
-        <>
-            <section className='container mx-auto my-5'>
-                <div className='text-center'>
-                    <h1 className='text-3xl font-bold '>Featured Jobs</h1>
-                    <p className='text-gray-700 my-4'>Explore thousands of job opportunities with all the information you need. Its your future</p>
-                </div>
-                <div className='flex justify-between mx-auto items-center gap-10'>
-                    <div className="card w-80 bg-base-200  ">
-                        <img src="./src/assets/Icons/accounts.png" alt="" className='card-body bg-base-300 m-4 rounded-lg w-24' />
+        <>    
+        
+                
+              
+                    <div className="card w-96 border-solid border-2 border-gray-200 ">
+                        <img src={logo} alt="" className='card-body  m-4 rounded-lg w-48' />
                         <div className="p-4">
-                            <h2 className="card-title text-2xl">Account & Finance</h2>
-                            <p className='text-gray-700'>200 Jobs Available</p>
+                            <h2 className="card-title text-2xl">{job_title}</h2>
+                            <p className='text-gray-700'>{company}</p>
+                            <div className='flex gap-4 items-center text-center my-3 '>
+                            <p className='text-blue-500 border-solid border-2 border-gray-200  w-24 rounded-lg p-1'>{job_type}</p>
+                            <p className='text-blue-500 border-solid border-2 border-gray-200 w-24 rounded-lg p-1' >{job_time}</p>
+                            </div>
+                            <p className='text-gray-700'>{address}</p>
+                            <p className='text-gray-700'>{salary}</p>
 
                         </div>
                     </div>
-
-                </div>
-            </section>
-
+                
+           
         </>
     );
 };
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useLoaderData } from 'react-router-dom'
 import Catagory from './Catagory';
 import FeaturedJobs from './FeaturedJobs';
 import HeroArea from './HeroArea';
 const Home = () => {
+    const jobs = useLoaderData();
     return (
         <>
             <section>
                 <HeroArea></HeroArea>
                 <Catagory></Catagory>
-                <FeaturedJobs></FeaturedJobs>
+
+
+                <div className='text-center container mx-auto my-5'>
+                    <h1 className='text-3xl font-bold '>Featured Jobs</h1>
+                    <p className='text-gray-700 my-4'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+                </div>
+                <div className='container mx-auto my-5'>
+                    <div className=' grid grid-cols-2 mx-auto gap-5'>
+                    {
+                        jobs.map(job => <FeaturedJobs key={job._id} job={job}>
+
+
+                        </FeaturedJobs>)
+                    }
+                    </div>
+                </div>
+
             </section>
         </>
     );
